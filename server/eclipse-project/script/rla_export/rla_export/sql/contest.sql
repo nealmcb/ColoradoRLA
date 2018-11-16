b23e65abdcd0f80e4056a30fd761ad1a834480e2
Neal McBurnett <nealmcb@freeandfair.us>
2018-11-15 17:12:49 -0700

    Add back contest_ballot_card_count
    Also round overstatements to an int, until it is changed back to an int in the database.
    And consistently use upper case for SQL keywords AS and ON.

--- server/eclipse-project/script/rla_export/rla_export/sql/contest.sql
+++ server/eclipse-project/script/rla_export/rla_export/sql/contest.sql
@@ -1,15 +1,15 @@
--- List of contests with current status.  Which contests has the  Secretary selected for audit? Which contests (if any) has the  Secretary selected for hand count? How many discrepancies of each type have been found so far?
-
+-- List of contests with basic info, current status and
+-- data needed for risk calculations.
 
 SELECT 
    cr.contest_name AS contest_name,
-   LOWER(ca.audit_reason) as audit_reason,
+   LOWER(ca.audit_reason) AS audit_reason,
    --LOWER(cta.audit) AS current_audit_type,
-   LOWER(ca.audit_status) as random_audit_status,
+   LOWER(ca.audit_status) AS random_audit_status,
    -- cn.votes_allowed,
    cr.winners_allowed,
    cr.ballot_count AS ballot_card_count,
-   -- cr.contest_ballot_count AS contest_ballot_card_count,   -- need to SUM from county_contest_results
+   agg.contest_ballot_card_count,
    SUBSTRING(cr.winners, 2, LENGTH(cr.winners) - 2) AS winners,
    cr.min_margin,
    ca.risk_limit,
@@ -21,7 +21,7 @@ SELECT
    ca.disagreement_count,
    ca.other_count,
    ca.gamma,
-   ca.overstatements,
+   ROUND(ca.overstatements, 0) AS overstatements,
    ca.optimistic_samples_to_audit,
    ca.estimated_samples_to_audit
 FROM 
@@ -34,5 +34,14 @@ FROM
 LEFT JOIN
    contest_result AS cr
    ON ca.contest_result_id = cr.id
+LEFT JOIN
+  (SELECT ctcr.contest_result_id,
+          SUM(contest_ballot_count) AS contest_ballot_card_count
+      FROM county_contest_result AS ccr
+      LEFT JOIN contests_to_contest_results AS ctcr
+      ON ccr.contest_id = ctcr.contest_id
+      GROUP BY ctcr.contest_result_id
+   ) AS agg
+  ON agg.contest_result_id = cr.id
 ORDER BY contest_name
 ;
