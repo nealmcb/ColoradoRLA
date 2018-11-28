548e799a83963e6db0adb86cc1dfbb140bde6849
Chris Thompson <cthompson@democracy.works>
2018-11-28 23:03:18 +0000

    [temp] turn on debug for reset button

--- client/webpack.config.prod.js
+++ client/webpack.config.prod.js
@@ -26,7 +26,7 @@ module.exports = {
 
     plugins: [
         new webpack.DefinePlugin({
-            DEBUG: false,
+            DEBUG: true,
         }),
 
         new webpack.NamedModulesPlugin(),
