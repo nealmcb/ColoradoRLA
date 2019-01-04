06465852ed1eab5b7d6935804a1fa9ae217fca41
Chris Thompson <cthompson@democracy.works>
2019-01-04 16:46:28 +0000

    [client] add Delete File button

---
+++ client/src/action/county/deleteFile.ts
@@ -0,0 +1,40 @@
+import action from 'corla/action';
+import { endpoint } from 'corla/config';
+import { empty } from 'corla/util';
+
+const deleteFileUrl = endpoint('delete-file');
+
+
+async function deleteFile(fileType: string) {
+    const init: RequestInit = {
+        body: JSON.stringify( { fileType } ),
+        credentials: 'include',
+        method: 'post',
+    };
+
+
+    try {
+        action('DELETE_FILE_SEND');
+
+        const r = await fetch(deleteFileUrl, init);
+
+        const received = await r.json().catch(empty);
+
+        if (!r.ok) {
+            action('DELETE_FILE_FAIL', received);
+            return;
+        }
+
+        action('DELETE_FILE_OK', received);
+
+
+    } catch (e) {
+        action('DELETE_FILE_NETWORK_FAIL');
+
+        throw e;
+
+    }
+
+}
+
+export default deleteFile;
