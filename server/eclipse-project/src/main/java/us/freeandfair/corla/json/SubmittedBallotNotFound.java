5001c4c1b7c3938a54e02b800439e63b88639b1d
Chris Thompson <cthompson@democracy.works>
2019-01-24 19:33:27 +0000

    [api] add reaudit flag to BallotNotFound endpoint

--- server/eclipse-project/src/main/java/us/freeandfair/corla/json/SubmittedBallotNotFound.java
+++ server/eclipse-project/src/main/java/us/freeandfair/corla/json/SubmittedBallotNotFound.java
@@ -30,12 +30,33 @@ public class SubmittedBallotNotFound {
    */
   public SubmittedBallotNotFound(final Long the_id) {
     my_id = the_id;
+    this.reaudit = false;
   }
-  
+
+  /**
+   * Constructs a new SubmittedBallotNotFound.
+   *
+   * @param the_id The id.
+   */
+  public SubmittedBallotNotFound(final Long the_id, final Boolean reaudit) {
+    my_id = the_id;
+    this.reaudit = reaudit;
+  }
+
+  /** flag to indicate whether this is a review-and-reaudit submission
+   * - needs to not be final so it can be optional and have a default
+   **/
+  private final Boolean reaudit;
+
   /**
    * @return the id.
    */
   public Long id() {
     return my_id;
+  }
+
+  /** reaudit can be null because it is optional **/
+  public Boolean isReaudit() {
+    return this.reaudit != null && this.reaudit;
   }
 }
