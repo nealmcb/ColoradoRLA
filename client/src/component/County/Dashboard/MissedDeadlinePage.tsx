05389833e17944223d5aca2d3de9c9f166655e9c
Gordon Stratton <gordon@democracy.works>
2019-05-16 18:19:34 -0700

    Re-style "missed deadline" page
    Just a small simplifcation to make this look a bit nicer and use a
    callout for its intended purpose.

--- client/src/component/County/Dashboard/MissedDeadlinePage.tsx
+++ client/src/component/County/Dashboard/MissedDeadlinePage.tsx
@@ -1,24 +1,18 @@
-
-
 import * as React from 'react';
 
-import { Card } from '@blueprintjs/core';
+import { Callout } from '@blueprintjs/core';
 
 import CountyLayout from 'corla/component/CountyLayout';
 import * as config from 'corla/config';
 
 const MissedDeadlinePage = () => {
     const main =
-        <div>
-            <h2>Upload Deadline Missed</h2>
-            <div>
-                <Card>
-                    You are unable to upload a file because the deadline has passed and the
-                    audit has begun. Please contact the CDOS voting systems team at&nbsp;
-                    <strong>{ config.helpEmail }</strong> or <strong>{ config.helpTel }</strong> for assistance.
-                </Card>
-            </div>
-        </div>;
+        <Callout icon='info-sign'
+                 title='Upload deadline missed'>
+            You are unable to upload a file because the deadline has passed and the
+            audit has begun. Please contact the CDOS voting systems team at&nbsp;
+            <strong>{ config.helpEmail }</strong> or <strong>{ config.helpTel }</strong> for assistance.
+        </Callout>;
 
     return <CountyLayout main={ main } />;
 };
